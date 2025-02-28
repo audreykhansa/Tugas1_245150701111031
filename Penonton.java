b0802733067622b51feeeac455e425777b0aacac
audreykhansa <audreykhsalra@gmail.com>
2025-02-28 08:35:08 +0700

    Create Penonton.java

---
+++ Penonton.java
@@ -0,0 +1,13 @@
+package TugasPraktikum1;
+
+public class Penonton {
+    private String namaPenonton;
+
+    public Penonton(String namaPenonton) {
+        this.namaPenonton = namaPenonton;
+    }
+
+    public String getNamaPenonton() {
+        return namaPenonton;
+    }
+}
